38ba592cddd12f36b33a24e449bc063f2f06eb0a
simon <moersimonlol123456789@gmail.com>
2023-05-27 12:17:58 +0200

    ssl update

--- public/mail/ssl/init_ssl.sh
+++ public/mail/ssl/init_ssl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # génération du certificat SSL
-openssl req -x509 -newkey rsa:4096 -keyout /etc/ssl/private/server.key -out /etc/ssl/certs/server.crt -days 3650 -nodes -subj "/CN=woodytoys.lab"
+openssl req -x509 -newkey rsa:4096 -keyout /etc/ssl/private/server.key -out /etc/ssl/certs/server.crt -days 3650 -nodes -subj "/CN=l2-4.ephec-ti.be"
 
 # copie des certificats dans le volume
 cp /etc/ssl/private/server.key /ssl_private/
